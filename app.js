3e32388e8fff8fd321154ea5e0bd8ced5d13c21a
springerkc <jspringer@packet.com>
2022-07-12 22:30:31 -0500

    init repo :rocket:

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const fs = require('fs');
+
+const { S3Client, ListObjectsCommand } = require('@aws-sdk/client-s3');
+
+(async () => {
+	const client = new S3Client({
+		region: process.env.AWS_REGION,
+		credentials: { accessKeyId: process.env.AWS_KEY, secretAccessKey: process.env.AWS_SEC },
+	});
+	const command = new ListObjectsCommand({ Bucket: process.env.AWS_BUCKET, MaxKeys: process.env.AWS_MAXKEYS });
+	const response = await client.send(command);
+	
+	fs.writeFileSync(`./_results/${process.env.AWS_BUCKET.toLowerCase()}_${Date.now()}_objectList.json`, JSON.stringify(response, null ,2));
+})()
