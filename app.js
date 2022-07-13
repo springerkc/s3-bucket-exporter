e1619e2ab3baca5e51fa9381f34e866cc33ad792
springerkc <jspringer@packet.com>
2022-07-12 23:05:22 -0500

    update env to reflect strings instead of values, especially for maxkeys

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { S3Client, ListObjectsCommand } = require('@aws-sdk/client-s3');
 		region: process.env.AWS_REGION,
 		credentials: { accessKeyId: process.env.AWS_KEY, secretAccessKey: process.env.AWS_SEC },
 	});
-	const command = new ListObjectsCommand({ Bucket: process.env.AWS_BUCKET, MaxKeys: process.env.AWS_MAXKEYS });
+	const command = new ListObjectsCommand({ Bucket: process.env.AWS_BUCKET, MaxKeys: "4000" });
 	const response = await client.send(command);
 	
 	fs.writeFileSync(`./_results/${process.env.AWS_BUCKET.toLowerCase()}_${Date.now()}_objectList.json`, JSON.stringify(response, null ,2));
